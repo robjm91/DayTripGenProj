511011d548b712339f9a321428d65597a186a991
robjm91 <robjmagana@gmail.com>
2020-10-19 18:14:27 -0700

    Created random entertainment function that successfully pulls random element from entertainment array.

--- DayTripGenCode.js
+++ DayTripGenCode.js
@@ -11,8 +11,8 @@
 // (5 points): As a developer, I want to make good, consistent commits.                  ***DONE
 // (5 points): As a user, I want a destination to be randomly selected for my day trip.   ***DONE
 // (5 points): As a user, I want a restaurant to be randomly selected for my day trip.     ***DONE
-// (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
-// (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+// (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. ***DONE
+// (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip. ***DONE
 // (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of
 // transportation, and/or form of entertainment if I don’t like one or more of those things.
 // (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the
@@ -58,6 +58,16 @@ function random_transportation(transportation) {
     return transportation[Math.floor(Math.random()*transportation.length)];   
 }
 console.log(random_transportation(transportation));
+
+//Random Form of Entertainment Generator
+let entertainment = ["Watch a Comedy Show", "Go Sight See", "Prank Innocent Bystander's", "Hit the bar!"];
+
+function random_entertainment(entertainment) {
+  
+    return entertainment[Math.floor(Math.random()*entertainment.length)];   
+}
+console.log(random_entertainment(entertainment));
+
 
 
 
